d3fa44804471180148a4a61e5b55b0121a9ac702
krishnaphad <phadkrishna94@gmail.com>
2022-04-07 21:31:31 +0530

    home page changes

---
+++ PHR.Services/EmailService/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using PHR.ViewModels.Common;
+using PHR.ViewModels.Login;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PHR.Services.EmailService
+{
+    public interface IEmailService
+    {
+        ResultViewModel SendForgotPasswordEmail(ForgotPasswordViewModel forgotPassword, string templatePath);
+        bool SendEmail(EmailParameters emailParams);
+    }
+}
